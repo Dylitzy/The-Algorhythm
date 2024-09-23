fdc0f729c8f9b15f3a779a845054a451a50c4a16
Dylan <dylanps4495@gmail.com>
2024-09-23 17:17:37 -0400

    - Found an API that works for me - Still figuring out how to extract the data I need

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import requests
 
 
+def make_search(search):
+    url = f"https://api.deezer.com/search?q={search}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()["data"]
+        print("We found results:")
+        print(data)
+    else:
+        print("An error has occurred accessing the API.")
+
+
 def make_list():
-    pass
+    creating = True
+    while creating:
+        search = input("Write the title of a song to search for it, or \"q\" to quit: ")
+        if creating == "q":
+            creating = False
+        else:
+            make_search(search)
 
 
 def main():
@@ -17,6 +34,7 @@ def main():
                 make_list()
             elif option == "2" or option.lower() == "option 2":
                 print("WIP")
+                # Genre, Similar Artist, Same Artist, Release Date, Band / Solo Artist, Instrumentation, Mood / Vibe
             elif option == "3" or option.lower() == "option 3" or option.startswith("q"):
                 print("See you later!")
                 break
